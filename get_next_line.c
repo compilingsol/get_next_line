bc25beae97c0ee5f04f2506f1c832994e8eed849
Sol Garcia <124470903+compilingsol@users.noreply.github.com>
2023-10-12 20:04:05 +0200

    WIP WIP WIP

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: maria-sg <maria-sg@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 14:37:39 by maria-sg          #+#    #+#             */
-/*   Updated: 2023/10/12 19:07:22 by maria-sg         ###   ########.fr       */
+/*   Updated: 2023/10/12 20:01:23 by maria-sg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,47 @@
 // remember the nulls!
 // size_t read(int fd, void *buf, size_t count)
 
-char	*readline(int fd, char **line, char **leftstr)
-{
-	char	*buff;
-	int	readbytes;
+// char	*readline(int fd, char **line, char **leftstr)
+// {
+// 	char	*buff;
+// 	int	readbytes;
 	
-	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buff)
-		return (NULL);
-	readbytes = read(fd, buff, BUFFER_SIZE);
-	if (readbytes <= 0)
-	{
-		free(buff);
-		return (NULL);
-	}
-	buff[readbytes] = '\0';
-	return (readbytes);
-}
+// 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+// 	if (!buff)
+// 		return (NULL);
+// 	readbytes = read(fd, buff, BUFFER_SIZE);
+// 	if (readbytes <= 0)
+// 	{
+// 		free(buff);
+// 		return (NULL);
+// 	}
+// 	buff[readbytes] = '\0';
+// 	return (readbytes);
+// }
 
 char	*get_next_line(int fd)
 {
-	
+	static char	*temp;
+	char	buffer[BUFFER_SIZE + 1];
+	int	bytesread;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	
+	if (temp && (ft_newline(temp) >= 0))
+		return ft_printline(&temp);
+	while ((bytesread = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[bytesread] = '\0';
+		temp = ft_strjoin(temp, buffer);
+		if (ft_newline(temp) >= 0)
+			break;
+	}
+	if (bytesread == -1)
+	{
+		free(temp);
+		temp = NULL;
+		return (NULL);
+	}
+	return (ft_printline(&temp));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,30 @@
 /*   By: maria-sg <maria-sg@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:08:12 by maria-sg          #+#    #+#             */
-/*   Updated: 2023/10/10 15:08:37 by maria-sg         ###   ########.fr       */
+/*   Updated: 2023/10/12 20:01:43 by maria-sg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// char	*ft_strchr(const char *s, int c)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (s[i] != (char)c)
+// 	{
+// 		if (s[i] == '\0')
+// 			return (NULL);
+// 		i++;
+// 	}
+// 	return ((char *)&s[i]);
+// }
+
+static int ft_newline(const char *str)
+{
+    int i = 0;
+    while (str[i] != '\0' && str[i] != '\n')
+        i++;
+    return i;
+}
